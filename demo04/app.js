8d7169786fb359ebe6bd2f846283375ac3218025
wy5211 <1579151657@qq.com>
2020-10-22 11:23:55 +0800

    搭建静态服务器

--- demo04/app.js
+++ demo04/app.js
@@ -2,26 +2,38 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 const path = require('path');
+const common = require('./module/common');
 
+http
+  .createServer((req, res) => {
+    let pathname = url.parse(req.url).pathname;
+    pathname = pathname === '/' ? 'index.html' : pathname;
+    console.log(path.extname(pathname));
 
-http.createServer((req, res) => {
+    let extname = path.extname(pathname);
 
-  let pathname = url.parse(req.url).pathname;
+    if (pathname !== '/favicon.ico') {
+      fs.readFile(`static/${pathname}`, async (err, data) => {
+        if (err) {
+          res.writeHead(404, {
+            'Content-Type': `${await common.asyncMIMEType(
+              extname,
+            )};charset=utf-8`,
+          });
+          res.write('404 页面不存在');
+          res.end();
+          return;
+        }
+        res.writeHead(200, {
+          'Content-Type': `${await common.asyncMIMEType(
+            extname,
+          )};charset=utf-8`,
+        });
+        res.write(data);
+        res.end();
+      });
+    }
+  })
+  .listen(3000);
 
-  console.log(path.extname(pathname));
-
-
-  if (pathname !== '/favicon.ico') {
-    fs.readFile(`static/${pathname}`, (err, data) => {
-      if (err) {
-        res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
-        res.end('404 页面不存在')
-        return;
-      }
-      res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
-      res.end(data)
-    })
-  }
-}).listen(3000)
-
-console.log('http://127.0.0.1:3000')
+console.log('http://127.0.0.1:3000');
--- demo04/module/common.js
+++ demo04/module/common.js
@@ -1,5 +1,26 @@
 exports.MIMEType = (pathname) => {
   switch (pathname) {
-    // case ''
+    case '.html':
+      return 'text/html';
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'text/javascript';
+    default:
+      return;
   }
-}
+};
+const fs = require('fs');
+
+// 读取文件操作是异步的
+exports.asyncMIMEType = (pathname) => {
+  return new Promise((resove, reject) => {
+    fs.readFile('./data/mime.json', (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      let mimeObjg = JSON.parse(data.toString());
+      resove(mimeObjg[pathname]);
+    });
+  });
+};
