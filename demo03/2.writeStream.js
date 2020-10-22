6790163048896fd96f834d59a149d19aa056cc6f
wy5211 <1579151657@qq.com>
2020-10-22 14:29:04 +0800

    b

--- demo03/2.writeStream.js
+++ demo03/2.writeStream.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-let str = ''
+let str = '';
 
 for (let i = 0; i < 1000; i++) {
-  str += 'writeSteam Test By 王阳'
+  str += 'writeSteam Test By 王阳';
 }
 
 const writeStream = fs.createWriteStream('./data/writeFile.txt');
@@ -11,8 +11,8 @@ const writeStream = fs.createWriteStream('./data/writeFile.txt');
 writeStream.write(str);
 
 // 结束标记，有了 end() 才会执行后面的 finish
-writeStream.end()
+writeStream.end();
 
 writeStream.on('finish', () => {
   console.log('写入完成');
-})
+});
