2865c54b2d9129976f3a8150240f184c55d133d8
wy5211 <1579151657@qq.com>
2020-10-28 09:04:09 +0800

    add

---
+++ express_study/demo05/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  /*
+    maxAge?: number;  过期时间/设置时长(毫秒)
+    signed?: boolean; 是否加密(默认false)
+    expires?: Date;   过期时间/设置data
+    httpOnly?: boolean; 设置为true js 无法获取cookie,只能在服务端操作
+    path?: string;    设置可以获取cookie 的path
+    domain?: string;  设置二级域名, eg: 设置 .wy.com 时, a.wy.com & b.wy.com 都可以访问cookie
+    secure?: boolean; 设置为 true 只有 https 可以设置/获取cookie
+    encode?: (val: string) => string; 
+    sameSite?: boolean | 'lax' | 'strict' | 'none';
+   */
+  // res.cookie("username", "wangyang", { maxAge: 1000 * 10 });
+  res.cookie("username", "王阳", { signed: true });
+  res.send("home page");
+});
+
+app.get("/cart", (req, res) => {
+  // 直接通过req.cookies 获取cookie
+  // const { username } = req.cookies;
+
+  const { username } = req.signedCookies;
+
+  res.send(`cart page ---  ${username}`);
+});
+
+app.listen(80, () => {
+  console.log("127.0.0.1");
+});
