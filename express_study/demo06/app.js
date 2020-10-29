6c9e552bdc0941bfea1ca784b66c37575c52ba39
wy5211 <1579151657@qq.com>
2020-10-29 08:58:15 +0800

    add

--- express_study/demo06/app.js
+++ express_study/demo06/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("home page");
