f9a656efe538ce8095bae3c107e3137be70242d7
wy5211 <1579151657@qq.com>
2020-11-06 17:12:14 +0800

    add

--- express_study/demo08/routes/admin/user.js
+++ express_study/demo08/routes/admin/user.js
@@ -1,41 +1,37 @@
+const { json } = require("body-parser");
 const express = require("express");
-const path = require('path');
 
 const route = express.Router();
 
-const multer = require('multer')
-// 方式一： 配置上传路径， 这种配置无法给文件命名
-// const upload = multer({ dest: 'static/upload' })
-
-// 方式二：可以给文件重命名
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'static/upload')
-  },
-  filename: function (req, file, cb) {
-    const extname = path.extname(file.originalname)
-    cb(null, file.fieldname+extname)
-  }
-})
+// 文件上传封装
+const tools = require('../../utils/tool');
  
-var upload = multer({ storage: storage })
-
 route.get("/", (req, res) => {
   res.render('admin.html', {})
 });
-
+ 
 route.get("/add", (req, res) => {
   res.send("admin 列表 增加");
 });
 
 route.get("/edit", (req, res) => {
   res.send("admin 列表 编辑");
-});
+}); 
 
-route.post("/do_add", upload.single('some_name'), (req, res) => {
+// 只上传一个文件
+// route.post("/do_add", upload.single('some_name'), (req, res) => {
+//   res.send({
+//     body: req.body,
+//     file: req.file // 通过 req.file 可以获取上传的文件
+//   });
+// });
+
+// 上传多个文件
+var cpUpload = tools.multer().fields([{ name: 'fileName1', maxCount: 1 }, { name: 'fileName2', maxCount: 1 }])
+route.post("/do_add", cpUpload, (req, res) => {
   res.send({
     body: req.body,
-    file: req.file // 通过 req.file 可以获取上传的文件
+    file: req.files // 上传多个文件，通过 req.files 可以获取上传的文件
   });
 });
 
