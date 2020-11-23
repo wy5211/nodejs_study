5ede9273424e41f6f12b95daaeb90aa5ff5b4ac1
wy5211 <1579151657@qq.com>
2020-11-23 09:06:32 +0800

    add

--- express_study/demo08/routes/admin/user.js
+++ express_study/demo08/routes/admin/user.js
@@ -1,22 +1,21 @@
-const { json } = require("body-parser");
 const express = require("express");
 
 const route = express.Router();
 
 // 文件上传封装
-const tools = require('../../utils/tool');
- 
+const tools = require("../../utils/tool");
+
 route.get("/", (req, res) => {
-  res.render('admin.html', {})
+  res.render("admin.html", {});
 });
- 
+
 route.get("/add", (req, res) => {
   res.send("admin 列表 增加");
 });
 
 route.get("/edit", (req, res) => {
   res.send("admin 列表 编辑");
-}); 
+});
 
 // 只上传一个文件
 // route.post("/do_add", upload.single('some_name'), (req, res) => {
@@ -27,11 +26,15 @@ route.get("/edit", (req, res) => {
 // });
 
 // 上传多个文件
-var cpUpload = tools.multer().fields([{ name: 'fileName1', maxCount: 1 }, { name: 'fileName2', maxCount: 1 }])
+var cpUpload = tools.multer().fields([
+  { name: "fileName1", maxCount: 1 },
+  { name: "fileName2", maxCount: 1 },
+]);
 route.post("/do_add", cpUpload, (req, res) => {
+  console.log(req.files);
   res.send({
     body: req.body,
-    file: req.files // 上传多个文件，通过 req.files 可以获取上传的文件
+    file: req.files, // 上传多个文件，通过 req.files 可以获取上传的文件
   });
 });
 
